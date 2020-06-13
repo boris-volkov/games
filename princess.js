b3376761574b3e042b7ac4180adf4ffb95174a06
boris-volkov <boris366@gmail.com>
2020-06-13 00:27:44 -0700

    fixed a bunch of stuff cause Hailey is here now

--- princess.js
+++ princess.js
@@ -1,11 +1,11 @@
 
-//prompt("got here");
+//prompt("got here"); // this is just a debugging thing, put this somewhere as a breakpoint
 
 function princess(input){
 	var head = input.charAt(0);
 	var tail = input.charAt(input.length - 1);
 	var rest = input.substring(1);
-	
+
 	if (head == "1" && tail == "2")
 		return input.substring(1,input.length - 1);
 	else if (head == "3")
@@ -46,88 +46,103 @@ function test6(input){
 }
 
 
+const reminder ="\n\rRule I                        "+green+"1"+white+"a"+green+"2"+white+" → a\n\r"+
+	"Rule II        if a → b, then "+green+"3"+white+"a → bb\n\r"+
+	"Rule III       if a → b, then "+green+"4"+white+"a → b↩\n\r"+
+	"Rule IV        if a → b, then "+green+"5"+white+"a → ◌b\n\r"+
+	"Rule V         if a → b, then "+green+"6"+white+"a → 1b\n\r"+
+	"Rule VI        if a → b, then "+green+"7"+white+"a → 2b\n\n\r"
+
+
 //TODO make these more engaging
 var prompts = 
-[
-	yellow + "The first test:\n\r"+
-	"Send the princess a number that would make her\n\r"+
-	"respond with the exact same number\n\r" + blue + 
-	"x→x\n\n\r" + white,
+	[
+		yellow + "The first test:\n\r"+
+		"Send the princess a number that would make her\n\r"+
+		"respond with the exact same number\n\r" + blue + 
+		"x→x\n\n\r" + white,
 
-	yellow + "The second test:\n\r"+ blue + 
-	"x→xx\n\n\r" + white,
+		yellow + "The second test:\n\r"+ blue + 
+		"x→xx\n\n\r" + white,
 
-	yellow + "the third test:\n\r"+ blue + 
-	"x→x'(digits reversed)\n\n\r" + white,
+		yellow + "the third test:\n\r"+ blue + 
+		"x→x'(digits reversed)\n\n\r" + white,
 
-	yellow + "the fourth test:\n\r"+ blue + 
-	"x→x'(with the final digit removed)\n\n\r",
+		yellow + "the fourth test:\n\r"+ blue + 
+		"x→x'(with the final digit removed)\n\n\r",
 
-	yellow + "the fifth test:\n\r"+ blue + 
-	"x→y→x find a cyclic element of order 2\n\n\r", // is that right Tim?
+		yellow + "the fifth test:\n\r"+ blue + 
+		"x→y→x find a cyclic element of order 2\n\n\r", // is that right Tim?
 
-	yellow + "the sixth test:\n\r"+ blue + 
-	"x→y→x'(with its digits reversed).\n\n\r"
-]
+		yellow + "the sixth test:\n\r"+ blue + 
+		"x→y→x'(with its digits reversed).\n\n\r"
+	]
 
 var tests = [test1, test2, test3, test4, test5, test6];
 
 $(function () {
-    var term = new Terminal({theme: {background: "#882255"}}, {cursorWidth: 100});
+	var term = new Terminal({theme: {background: "#882255"}}, {cursorWidth: 100});
 
-    term.setOption("fontSize", 40);
-    term.setOption("fontWeight", 900);
-    //term.setOption("fontFamily", "Ubuntu Mono");
-    term.open(document.getElementById('terminal'));
+	term.setOption("fontSize", 40);
+	term.setOption("fontWeight", 900);
+	//term.setOption("fontFamily", "Ubuntu Mono");
+	term.open(document.getElementById('terminal'));
+	const princess_prompt = '\r\nSend her a number...  ';
 
-    function runFakeTerminal() {
-	var i = 0;
-        
-	if (term._initialized) {
-            return;
-        }
+	function runFakeTerminal() {
+		var i = 0;
 
-        term._initialized = true;
-
-        term.prompt = () => {
-            term.write('\r\nSend her a number...  ');
-        };
-	//term.write('\x1b[48;2;30;60;90m')
-	term.write('\x1b[97m');
-        term.writeln(prompts[i]);
-        prompt(term);
-	
-        var buffer = "";
-        
-        term.onKey(e => {
-            const printable = !e.domEvent.altKey && !e.domEvent.altGraphKey && !e.domEvent.ctrlKey && !e.domEvent.metaKey;
-
-            if (e.domEvent.keyCode === 13) {
-		if (buffer == "") return;
-		term.write("\r\n\x1b[94m" +  "The princess returns: " + princess(buffer) + "\x1b[97m");
-		if (tests[i](buffer)){
-			i++;
-			term.write("\x1b[91m\r\nYou passed Test " + i + "!\x1b[97m\r\n\n");
-			term.write(prompts[i]);
+		if (term._initialized) {
+			return;
 		}
-	        buffer = "";
-                prompt(term);
-            } else if (e.domEvent.keyCode === 8) {
-                // Do not delete the prompt
-                if (term._core.buffer.x > 2) {
-		    buffer = buffer.substring(0, buffer.length - 1);
-                    term.write('\b \b');
-                }
-            } else if (printable) {
-		buffer = buffer + e.key;
-                term.write(e.key);
-            }
-        });
-    }
 
-    function prompt(term) {
-      	term.write('\r\nSend her a number...  ');
-    }
-    
-    runFakeTerminal();
+		term._initialized = true;
+
+		term.prompt = () => {
+			term.write(princess_prompt);
+		};
+		//term.write('\x1b[48;2;30;60;90m')
+		term.write('\x1b[97m');
+		term.writeln(prompts[i]);
+		prompt(term);
+
+		var buffer = "";
+
+		term.onKey(e => {
+			const printable = !e.domEvent.altKey && !e.domEvent.altGraphKey && !e.domEvent.ctrlKey && !e.domEvent.metaKey;
+
+			if (e.domEvent.keyCode === 13) {
+				if (buffer == "") return;
+				if (buffer == "?"){
+					term.write(reminder);
+					prompt(term);
+					buffer = "";
+					return
+				}
+				term.write("\r\n\x1b[94m" +  "The princess returns: " + princess(buffer) + "\x1b[97m");
+				if (tests[i](buffer)){
+					i++;
+					term.write("\x1b[91m\r\nYou passed Test " + i + "!\x1b[97m\r\n\n");
+					term.write(prompts[i]);
+				}
+				buffer = "";
+				prompt(term);
+			} else if (e.domEvent.keyCode === 8) {
+				// Do not delete the prompt
+				if (term._core.buffer.x > princess_prompt.length - 2) {
+					buffer = buffer.substring(0, buffer.length - 1);
+					term.write('\b \b');
+				}
+			} else if (printable) {
+				buffer = buffer + e.key;
+				term.write(e.key);
+			}
+		});
+	}
+
+	function prompt(term) {
+		term.write(princess_prompt);
+	}
+
+	runFakeTerminal();
 });
--- rules.js
+++ rules.js
@@ -8,29 +8,31 @@ const white = '\x1b[97m';
 const yellow = '\x1b[93m';
 const blue = '\x1b[94m';
 const red = '\x1b[91m';
+const green = '\x1b[92m';
 
 const INTRODUCTION = 	"There is a princess who lives far away.\n\r" + 
-		    	"She has a way with numbers, by the way.\n\r" + 
-			"She has this game she plays, a number game:\n\r"+
-			"You send her a number, she'll do the same:\n\r"+
-			"Her number will be in response to yours.\n\r"+
-			"And yes, She'll teach you how it works, of course.\n\r"+
-			"She will reward the one that meets her test,\n\r"+
-			"Just learn six rules and figure out the rest.\n\r"+
-			"The princess is waiting.\n\r"+ 
-			"First she will teach you the rules:\n\n\n\r";
+	"She has a way with numbers, by the way.\n\r" + 
+	"She has this game she plays, a number game:\n\r"+
+	"You send her a number, she'll do the same:\n\r"+
+	"Her number will be in response to yours.\n\r"+
+	"And yes, She'll teach you how it works, of course.\n\r"+
+	"She will reward the one that meets her test,\n\r"+
+	"Just learn six rules and figure out the rest.\n\r"+
+	"The princess is waiting.\n\r"+ 
+	"First she will teach you the rules...\n\n\r"+
+	"Tell the princess you are ready:"
 
 const rules = 
 	[
 		yellow + "Rule 0: notation:\n\r"+ white + 
-		"-A single letter will be used to represent\n\r"+
+		"-A single letter can be used to represent\n\r"+
 		"Either a single-digit number, or a multi-digit number\n\r"+
 		"not just digits, so x can be 2 or it can be 1234\n\r"+
 		"-No letter has any special meaning,\n\r"+
-		"they are all just variables\n\r"+
+		"they are all just variables (a,b,x)\n\r"+
 		"-When you see variables next to each other,\n\r"+
 		"it means concatenation, not multiplication\n\r"+
-		"so if a = 543 and b = 987 ab means 5439878\n\r"+
+		"so if a = 543 and b = 987 ab means 543987\n\r"+
 		"and 3a means 3543\n\r"+
 		"-a → b means: when you send the princess a,\n\r"+
 		"she will respond with b.\n\n\r"+
@@ -54,28 +56,44 @@ const rules =
 		"                   x → 123123\n\n\n\r",
 
 		yellow + "RULE III: reversal\n\r"+ white + 
-		"		(if a→b, then 4a → b*(with its digits reversed))\n\r"+
+		"		(if a→b, then 4a → b↩ (b with its digits reversed))\n\r"+
 		"Example:      since 19872 → 987\n\r"+
 		"		    419872 → 789\n\r"+
 		"Use rule III to make her send back 123\n\r",
 
 		yellow + "Rule IV: ereasure\n\r"+ white + 
-		"(if a→b, then 5a→ b*(with the first digit removed))\n\r"+
+		"(if a→b, then 5a→ ◌b (b with the first digit removed))\n\r"+
 		"Example:      since 127432 → 2743\n\r"+
 		"		    5127432 → 743\n\r"+
 		"Use rule IV to make her send back 375\n\r",
 
-		yellow + "Rule V: addition    (if a→b then 6a→1b)\n\r"+ white + 
+		yellow + "Rule V: addition(1) (if a→b then 6a→1b)\n\r"+ white + 
 		"Example:      since 15552 → 555\n\r"+
 		"                   615552 → 1555\n\r"+
 		"Use rule V to return 1919\n\r",
 
-		yellow + "Rule VI: addition(2) (if a→b then 7a→2b\n\r"+ white +
+		yellow + "Rule VI: addition(2) (if a→b then 7a→2b)\n\r"+ white +
 		"Example:      since 13432 → 343\n\r"+
 		" 		    713432 → 2343\n\r"+
 		"Use rule VI ro return 222\n\r",
 
-		red + "End of the rules\n\n\r"
+		yellow + "Rule ∞: rules can be used in any combination\n\r"+ white +
+		"Example:      since   14342 →  434\n\r"+
+		"               and   614342 → 1434       (addition(1))\n\r"+
+		"              then  3614342 → 14341434   (doubling)\n\r"+
+		"Use rules IV(ereasure) and II(doubling) to get the \n\r"+
+		"princess to send you the number 47747\n\r",
+
+		red + "And those are all the rules.\n\n\r" + white + 
+		"Do not forget them:\n\n\n\r"+
+		"Rule I                        "+green+"1"+white+"a"+green+"2"+white+" → a\n\r"+
+		"Rule II        if a → b, then "+green+"3"+white+"a → bb\n\r"+
+		"Rule III       if a → b, then "+green+"4"+white+"a → b↩\n\r"+
+		"Rule IV        if a → b, then "+green+"5"+white+"a → ◌b\n\r"+
+		"Rule V         if a → b, then "+green+"6"+white+"a → 1b\n\r"+
+		"Rule VI        if a → b, then "+green+"7"+white+"a → 2b\n\n\r"+
+		"Now you are ready for the tests ↴\n\r"+
+		"Send the princess a ? if you need a reminder..."
 	]
 
 // this is how we make sure they understand the rules
@@ -86,7 +104,8 @@ const asserts =
 		["4", "123"],
 		["5", "375"],
 		["6", "1919"],
-		["7", "222"]
+		["7", "222"],
+		["53", "47747"]
 	]
 
 /**
@@ -98,68 +117,75 @@ function test(input, i){
 
 
 $(function () {
-    var term = new Terminal({theme: {background: "#006699"}}, {cursorWidth: 100});
+	var term = new Terminal({theme: {background: "#006699"}}, {cursorWidth: 100});
 
-    term.setOption("fontSize", 40);
-    term.setOption("fontWeight", 900);
-    //term.setOption("fontFamily", "Ubuntu Mono");
-    term.open(document.getElementById('terminal'));
+	term.setOption("fontSize", 30);
+	term.setOption("fontWeight", 900);
+	//term.setOption("fontFamily", "Ubuntu Mono");
+	term.open(document.getElementById('terminal'));
 
-    function runFakeTerminal() {
-        if (term._initialized) {
-            return;
-        }
-
-        term._initialized = true;
-
-        term.prompt = () => {
-            term.write('\r\nType it here: ');
-        };
-	//term.write('\x1b[48;2;30;60;90m')
-	term.write('\x1b[97m');
-        term.write(INTRODUCTION);
-
-	var i = 0;
-        term.writeln(rules[i]);
-        prompt(term);
+	const term_prompt = '\r\nTry a number: ';
 	
-        var buffer = "";
-        
-        term.onKey(e => {
-            const printable = !e.domEvent.altKey && !e.domEvent.altGraphKey && !e.domEvent.ctrlKey && !e.domEvent.metaKey;
+	function runFakeTerminal() {
+		if (term._initialized) {
+			return;
+		}
 
-            if (e.domEvent.keyCode === 13) 
-	    {
-		if (buffer == "") return;
-	    if (buffer.toLowerCase().includes("i understand"))
-		term.dispose();
-	    term.writeln("\n\rthe princess would return: " + princess(buffer));
-	    if (test(buffer, i))
-	    {
-		    i++;
-		    term.writeln(red + "\n\n\n    Correct\n\n\n\n\r");
-		    term.writeln(rules[i]);
-	    }
-	        buffer = "";
-                prompt(term);
-	    if (i > rules.length)
-		    term.dispose();
-            } else if (e.domEvent.keyCode === 8) {
-                // Do not delete the prompt
-                if (term._core.buffer.x > 2) {
-		    buffer = buffer.substring(0, buffer.length - 1);
-                    term.write('\b \b');
-                }
-            } else if (printable) {
-		buffer = buffer + e.key;
-                term.write(e.key);
-            }
-        });
-    }
 
-    function prompt(term) {
-            term.write('\r\nType it here: ');
-    }
-    
-    runFakeTerminal();
+		term._initialized = true;
+
+		term.write('\x1b[97m'); // sets text color
+		term.write(INTRODUCTION);
+
+		var i = 0; 		// index to iterate through the rules
+		started = false;
+
+		var buffer = ""; // initialize an empty buffer. I think this can be accessed 
+				 // through term._core.buffer instead of building manually
+
+		term.onKey(e => {
+			const printable = !e.domEvent.altKey && !e.domEvent.altGraphKey && !e.domEvent.ctrlKey && !e.domEvent.metaKey;
+			if (started == false) {
+				term.reset();
+				term.writeln(rules[0]);
+				prompt(term);
+				started = true;
+				return;
+			}
+			if (e.domEvent.keyCode === 13) 
+			{
+				if (buffer == "") return; // don't let them send an empty string
+
+				if (i == rules.length-1) term.dispose(); //done learning rules.
+
+				term.writeln("\n\rthe princess would return: " + princess(buffer));
+				if (test(buffer, i)){
+					i++;
+					term.writeln(red + "\n\n\n    Correct\n\n\n\n\r");
+					term.writeln(rules[i]);
+					if (i == rules.length - 1) return; // so we don't prompt after the rules print out
+								           // is it stupid to check this twice? probably.		
+				}
+
+				buffer = "";
+				prompt(term);
+
+			} else if (e.domEvent.keyCode === 8) { // backspace
+				// Do not delete the prompt
+				if (term._core.buffer.x > term_prompt.length - 2) {
+					buffer = buffer.substring(0, buffer.length - 1);
+					term.write('\b \b');
+				}
+			} else if (printable) {
+				buffer = buffer + e.key;
+				term.write(e.key);
+			}
+		});
+	}
+
+	function prompt(term) {
+		term.write(term_prompt);
+	}
+
+	runFakeTerminal();
 });
