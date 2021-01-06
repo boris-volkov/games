a16e6c95184a3692394327063fb7258a11423116
boris-volkov <boris366@gmail.com>
2021-01-05 19:19:20 -0800

    small edits

--- code/multiplication/multiply.py
+++ code/multiplication/multiply.py
@@ -1,26 +1,23 @@
 import time, random
 
-deck = []             # empty list for the deck
-for i in range(1,13): # fill the deck with cards
-    for j in range(i, 13):
-        deck.append( (i, j, i*j) )
+deck = []                               # empty list for the deck
+for a in range(1,13):                   # fill the deck with cards
+    for b in range(i, 13):
+        deck.append( (a, b, a*b) )      # pack
 
-for _ in range(1000): # 1000 swaps to scramble
-    a = random.randint(1, len(deck)-1)
-    b = random.randint(1, len(deck)-1)
-    deck[a], deck[b] = deck[b], deck[a] # python swap
+for _ in range(1000):                   # 1000 swaps to scramble
+    i = random.randint(1, len(deck)-1)  # pick two random
+    j = random.randint(1, len(deck)-1)  # slots in the deck
+    deck[i], deck[j] = deck[j], deck[i] # swap
 
-start_time = time.time()
+start_time = time.time()                
 
 for i in range(len(deck)):
-    a, b, answer = deck[i] 
-    prompt = str(a) + ' x ' + str(b) + ' = '
-
-    # setting initial false value to 
-    # get the loop going:
-    my_answer = -1  
-    while my_answer != str(answer):
-        my_answer = input(prompt)
+    a, b, answer = deck[i]              # unpack
+    question = str(a)+' x '+str(b)+' = '
+    my_answer = -1                      # initial false value to
+    while my_answer != str(answer):     # get the loop started
+        my_answer = input(question)
 
 time_taken = time.time() - start_time
 
