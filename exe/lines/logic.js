354941b0debc976225ed33c0ff66470324bc68c8
boris-volkov <boris366@gmail.com>
2021-01-08 16:51:42 -0800

    readme

--- exe/lines/logic.js
+++ exe/lines/logic.js
@@ -1,7 +1,5 @@
 
 
-
-
 function findLineByLeastSquares(values_x, values_y) {
     let x_sum = 0;
     let y_sum = 0;
@@ -46,3 +44,61 @@ function findLineByLeastSquares(values_x, values_y) {
 
     return [m, b, result_values_x, result_values_y];
 }
+
+
+function ideal_line(m , b){
+	return 0;
+}
+
+var canvas = document.querySelector('#canvas');
+canvas.width = 1200;
+canvas.height = 600;
+var context = canvas.getContext('2d');
+context.fillRect(0,0,canvas.width, canvas.height);
+
+
+//-------------------------------------------------------stylus drawing listeners/handlers
+let drawing_mode = true;
+let pen_down = false;
+let bb; // bounding box, to adjust x,y coordinates within the terminal.
+
+let drawn_points_x = []
+let drawn_points_y = []
+
+canvas.onpointerdown = (event) => {
+	if (drawing_mode){
+		pen_down = true;
+		bb = canvas.getBoundingClientRect(); 
+		let x = (event.clientX-bb.left)*(canvas.width/bb.width);
+		let y = (event.clientY-bb.top)*(canvas.height/bb.height);
+		context.moveTo(x,y);
+		context.strokeStyle = "#369";
+	}
+}
+
+canvas.onpointermove = (event) => {
+	if (pen_down){
+			let x = (event.clientX-bb.left)*(canvas.width/bb.width);
+			let y = (event.clientY-bb.top)*(canvas.height/bb.height);
+			drawn_points_x.push(x)
+			drawn_points_y.push(y)
+			context.lineTo(x,y);
+			context.stroke();
+			context.moveTo(x,y);
+	}
+}
+
+canvas.onpointerup = () => {
+		context.closePath();
+		pen_down = false;
+}
+
+
+// next stuff prevents touch scrolling on mobile/ipad
+
+function preventDefault(e){
+    e.preventDefault();
+}
+document.body.addEventListener('touchmove', preventDefault, { passive: false });
+
+context.fillStyle = "#000"
---
+++ readme.txt
@@ -0,0 +1,13 @@
+This page is meant to display code in an aesthetic way. 
+
+Each language has its own color scheme, and features syntax hilighting.
+The color scheme is controlled by the css, 
+and the syntax hilighting is done via a python parser program that puts in html tags. 
+With this program I can quickly generate new pages out of any code that I have. 
+
+The full code for this page is shown below, some parts of it may be a little messy,
+but overall, it works. There is also code for the terminal interface to this page, 
+which is a very good example of spaghetti code!
+
+Slowly read the code and try to see what it does on the actual webpage you're on.
+Sorry if it's complicated! If you find any mistakes in the code please let me know!
