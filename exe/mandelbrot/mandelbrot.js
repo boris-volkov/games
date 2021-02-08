88a95d5535bc41bfabe57a85753aad3cdc450cb2
boris-volkov <boris366@gmail.com>
2021-02-07 21:06:45 -0800

    mandelbrot

--- exe/mandelbrot/mandelbrot.js
+++ exe/mandelbrot/mandelbrot.js
@@ -113,236 +113,243 @@ const ROWS = 3;
 const COLS = 4;
 const NUMWORKERS = navigator.hardwareConcurrency || 2;
 
-class MandelbrotCanvas {
-	constructor(canvas) {
-		this.canvas = canvas;
-		this.context = canvas.getContext("2d");
-		this.workerPool = new WorkerPool(NUMWORKERS, "worker.js");
+// initialization (used to be constructor)
+const canvas = document.querySelector("#canvas");
+const context = canvas.getContext("2d");
+let workerPool = new WorkerPool(NUMWORKERS, "worker.js");
 
-		this.tiles = null;
-		this.pendingRender = null;
-		this.wantsRerender = false;
-		this.resizeTimer = null;
-		this.colorTable = null;
+let tiles = null;
+let pendingRender = null;
+let wantsRerender = false;
+let resizeTimer = null;
+let colorTable = null;
+let width = canvas.width = window.innerWidth;
+let height = canvas.height = window.innerHeight;
 
-		this.canvas.addEventListener("pointerdown", e => this.handlePointer(e));
-		window.addEventListener("keydown", e => this.handleKey(e));
-//		window.addEventListener("resize", e => this.handleResize(e));
-		window.addEventListener("popstate", e => this.setState( e.state, false));
+window.addEventListener("keydown", e => handleKey(e));
+window.addEventListener("resize", e => handleResize(e));
+window.addEventListener("popstate", e => setState( e.state, false));
 
-		this.state = 
-			PageState.fromURL(window.location) || PageState.initializeState();
-		history.replaceState(this.state, "", this.state.toURL());
+let state = PageState.fromURL(window.location) || PageState.initializeState();
+console.log(state);
+history.replaceState(state, "", state.toURL()); // probably don't want this
 
-		this.setSize();
+function setSize(){
+	width = canvas.width;
+	height = canvas.height;
+	tiles = [...Tile.tiles(width, height, ROWS, COLS)]
+	console.log("tiles", tiles)
+}
 
-		this.render();
-	}
 
-	setSize() {
-		this.width = this.canvas.width = window.innerWidth;
-		this.height = this.canvas.height = window.innerHeight;
-		this.tiles = [...Tile.tiles(this.width, this.height, ROWS, COLS)]
-	}
 
-	setState(f, save=true) {
-		if (typeof f === "function") {
-			f(this.state);
-		} else {
-			for(let property in f) {
-				this.state[property] = f[property];
-			}
-		}
-
-		this.render();
-
-		if (save) {
-			history.pushState(this.state, "", this.state.toURL());
+function setState(f, save=true) {
+	if (typeof f === "function") {
+		f(state);
+	} else {
+		for(let property in f) {
+			state[property] = f[property];
 		}
 	}
 
-	color(iterations, maxIterations) {
-		let alpha = 255;
-		let blue   = Math.round(75*Math.sin(iterations**0.4   + 2*Math.PI/3) + 175)
-		let green  = Math.round(75*Math.sin(iterations**0.4  - 2*Math.PI/3) + 75)
-		let red    = Math.round(-125*Math.sin(iterations**0.4   ) + 125)
-		//scale values to 0-255 range and return like this:
-		return ((alpha<<24) + (blue<<16) + (green<<8) + (red));
-	}
+	render();
 
-	render(){
-
-		if ( this.pendingRender) {
-			this.wantsRerender = true;
-			return;
-		}
-
-		let {cx, cy, perPixel, maxIterations} = this.state;
-		let x0 = cx - perPixel*this.width/2;
-		let y0 = cy - perPixel*this.height/2;
-
-		let promises = this.tiles.map( tile => this.workerPool.addWork({
-			tile: tile,
-			x0: x0 + tile.x * perPixel,
-			y0: y0 + tile.y * perPixel,
-			perPixel: perPixel,
-			maxIterations: maxIterations
-		}));
-
-		this.pendingRender = Promise.all(promises).then(responses => {
-			let min = maxIterations;
-			let max = 0;
-			for (let r of responses) {
-				if (r.min < min) min = r.min;
-				if (r.max > max) max = r.max;
-			}
-
-			// every iteration count from 0-maxIterations has its own color
-			if (!this.colorTable || this.colorTable.length !== maxIterations+1){
-				this.colorTable = new Uint32Array(maxIterations+1);
-			}
-
-			if (min === max) { // 
-				if (min === maxIterations) {
-					this.colorTable[min] = 0xFF000000;
-				} else {
-					this.colorTable[min] = 0xFF000000;
-				}
-			} else { //TODO change this! transparency is slow and it looks bad too
-				let maxlog = Math.log(1+max-min);
-				console.log(min, max);
-				for (let i = min; i < max; i++) {
-					this.colorTable[i] = this.color(i, maxIterations);
-				}
-			}
-
-			for (let r of responses) {
-				let iterations = new Uint32Array(r.imageData.data.buffer);
-				for (let i= 0; i < iterations.length; i++) {
-					iterations[i] = this.colorTable[iterations[i]];
-				}
-			}
-			this.canvas.style.transform = "";
-			for (let r of responses) {
-				this.context.putImageData(r.imageData, r.tile.x, r.tile.y);
-			}
-		})
-		.catch((reason) => {
-			console.error("promise rejected in render():", reason);
-		})
-		.finally(() => {
-			this.pendingRender = null;
-			if (this.wantsRerender) {
-				this.wantsRerender = false;
-				this.render();
-			}
-		});
-	}
-
-	handleResize(event) {
-		if (this.resizeTimer) clearTimeout(this.resizeTimer);
-		this.resizeTimer = setTimeout(() => {
-			this.resizeTimer = null;
-			this.setSize();
-			this.render();
-		}, 50);
-	}
-
-	handleKey(event) {
-		switch (event.key) {
-			case "Escape":
-				this.setState(PageState.initializeState());
-				break;
-			case "+":
-				this.setState(s => {
-					s.maxIterations = Math.round(s.maxIterations *1.5);
-				});
-				break;
-			case "-":
-				this.setState(s => {
-					s.maxIterations = Math.round(s.maxIterations/1.5);
-					if( s.maxIterations < 1) s.maxIterations = 1;
-				});
-				break;
-			case "o":
-				this.setState(s => s.perPixel *= 2);
-				break;
-			case "ArrowUp":
-				event.preventDefault();
-				this.up();
-				break;
-			case "ArrowDown":
-				event.preventDefault();
-				this.down();
-				break;
-			case "ArrowLeft":
-				this.left();
-				break;
-			case "ArrowRight":
-				this.right();
-				break;
-		}
-	}
-
-
-	up(){	
-		this.setState(s => s.cy -= this.height/10 * s.perPixel);
-	}
-
-	down() {
-		this.setState(s => s.cy += this.height/10 * s.perPixel);
-	}
-
-	left() {
-		this.setState(s => s.cx -= this.width/10 * s.perPixel);
-	}
-
-	right() {
-		this.setState(s => s.cx += this.width/10 * s.perPixel);
-	}
-
-
-
-	handlePointer(event) {
-		const x0 = event.clientX, y0 = event.clientY, t0 = Date.now();
-		
-		const pointerMoveHandler = event => {
-			let dx = event.clientX-x0;
-			let dy = event.clientY-y0; 
-			let dt = Date.now() - t0;
-			if (dx > 10 || dy > 10 || dt > 500){
-				this.canvas.style.transform = 'translate(${dx}px, ${dy}px)';
-			}
-		};
-
-		const pointerUpHandler = event => {
-			this.canvas.removeEventListener("pointermove", pointerMoveHandler);
-			this.canvas.removeEventListener("pointerup", pointerUpHandler);
-
-			const dx = event.clientX-x0, dy = event.clientY-y0, dt=Date.now()-t0;
-			const {cx, cy, perPixel} = this.state;
-
-			if (dy > 10 || dy > 10 || dt > 500) {
-				this.setState({cx:cx - dx*perPixel, cy: cy - dy*perPixel});
-			} else {
-				let cdx = x0 - this.width/2;
-				let cdy = y0 - this.height/2;
-
-				this.canvas.style.transform = 
-					'translate(${-cdx*2}px, ${-cdy*2}px) scale(2)';
-
-				this.setState(s => {
-					s.cx += cdx*s.perPixel;
-					s.cy += cdy*s.perPixel;
-					s.perPixel /= 2;
-				});
-			}
-		};
-
-		this.canvas.addEventListener("pointermove", pointerMoveHandler);
-		this.canvas.addEventListener("pointerup", pointerUpHandler);
+	if (save) {
+		history.pushState(state, "", state.toURL());
 	}
 }
 
-let canvas = document.querySelector("#canvas");
-new MandelbrotCanvas(canvas);
+function color(iterations) {
+	let alpha = 255;
+	let blue   = Math.round(75*Math.sin(iterations**0.4  + 2*Math.PI/3) + 175)
+	let green  = Math.round(75*Math.sin(iterations**0.4  - 2*Math.PI/3) + 75)
+	let red    = Math.round(-125*Math.sin(iterations**0.4   ) + 125)
+	//scale values to 0-255 range and return like this:
+	return ((alpha<<24) + (blue<<16) + (green<<8) + (red));
+}
+
+function render(){
+	console.log(workerPool);
+	if (pendingRender) {
+		wantsRerender = true;
+		return;
+	}
+
+	let {cx, cy, perPixel, maxIterations} = state;
+	let x0 = cx - perPixel*width/2;
+	let y0 = cy - perPixel*height/2;
+
+	let promises = tiles.map( tile => workerPool.addWork({
+		tile: tile,
+		x0: x0 + tile.x * perPixel,
+		y0: y0 + tile.y * perPixel,
+		perPixel: perPixel,
+		maxIterations: maxIterations
+	}));
+
+	pendingRender = Promise.all(promises).then(responses => {
+		let min = maxIterations;
+		let max = 0;
+		for (let r of responses) {
+			if (r.min < min) min = r.min;
+			if (r.max > max) max = r.max;
+		}
+
+		// every iteration count from 0-maxIterations has its own color
+		if (!colorTable || colorTable.length !== maxIterations+1){
+			colorTable = new Uint32Array(maxIterations+1);
+		}
+		if (min === max) { // 
+			if (min === maxIterations) {
+				colorTable[min] = 0xFF000000;
+			} else {
+				colorTable[min] = 0xFF000000;
+			}
+		} else { //TODO change this! transparency is slow and it looks bad too
+			for (let i = min; i < max; i++) {
+				colorTable[i] = color(i);
+			}
+		}
+		
+
+		for (let r of responses) {
+			let iterations = new Uint32Array(r.imageData.data.buffer);
+			for (let i= 0; i < iterations.length; i++) {
+				iterations[i] = colorTable[iterations[i]];
+			}
+		}
+		canvas.style.transform = "";
+		for (let r of responses) {
+			context.putImageData(r.imageData, r.tile.x, r.tile.y);
+		}
+	})
+	.catch((reason) => {
+		console.error("promise rejected in render():", reason);
+	})
+	.finally(() => {
+		pendingRender = null;
+		if (wantsRerender) {
+			wantsRerender = false;
+			render();
+		}
+	});
+}
+
+function handleResize(event) {
+	if (resizeTimer) clearTimeout(resizeTimer);
+		resizeTimer = setTimeout(() => {
+		resizeTimer = null;
+		setSize();
+		render();
+	}, 50);
+}
+
+function handleKey(event) {
+	switch (event.key) {
+		case "Escape":
+			reset();
+			break;
+		case "+":
+			iter_up();
+			break;
+		case "-":
+			iter_down();
+			break;
+		case "o":
+			out();
+			break;
+		case "ArrowUp":
+			event.preventDefault();
+			up();
+			break;
+		case "ArrowDown":
+			event.preventDefault();
+			down();
+			break;
+		case "ArrowLeft":
+			left();
+			break;
+		case "ArrowRight":
+			right();
+			break;
+	}
+}
+
+const reset_button = document.querySelector("#reset");
+function reset() {
+	setState(PageState.initializeState());
+}
+reset_button.onclick = reset;
+
+const iter_down_button = document.querySelector("#minus");
+const iter_up_button = document.querySelector("#plus");
+
+function iter_down() {
+	setState(s => {
+		s.maxIterations = Math.round(s.maxIterations/1.5);
+		if( s.maxIterations < 1) s.maxIterations = 1;
+	});
+}
+iter_down_button.onclick = iter_down;
+
+function iter_up() {
+	setState(s => {
+		s.maxIterations = Math.round(s.maxIterations *1.5);
+	});
+}
+iter_up_button.onclick = iter_up;
+
+
+const up_button = document.querySelector("#up");
+const down_button = document.querySelector("#down");
+const left_button = document.querySelector("#left");
+const right_button = document.querySelector("#right");
+const out_button = document.querySelector("#out");
+
+function up(){	
+	setState(s => s.cy -= height/10 * s.perPixel);
+}
+up_button.onclick = up;
+
+function down() {
+	setState(s => s.cy += height/10 * s.perPixel);
+}
+down_button.onclick = down;
+
+function left() {
+	setState(s => s.cx -= width/10 * s.perPixel);
+}
+left_button.onclick = left;
+
+function right() {
+	setState(s => s.cx += width/10 * s.perPixel);
+}
+right_button.onclick = right;
+
+function out() {
+	setState(s => s.perPixel *= 2);
+}
+out_button.onclick = out;
+
+canvas.addEventListener("pointerup", event => {
+	bb = canvas.getBoundingClientRect(); 
+	let x0 = (event.clientX-bb.left)*(canvas.width/bb.width);
+	let y0 = (event.clientY-bb.top)*(canvas.height/bb.height);
+	t0 = Date.now();
+
+	let {cx, cy, perPixel} = state;
+
+	let cdx = x0 - width/2;
+	let cdy = y0 - height/2;
+
+	canvas.style.transform = 'translate(${-cdx*2}px, ${-cdy*2}px) scale(2)';
+
+	setState(s => {
+		s.cx += cdx*s.perPixel;
+		s.cy += cdy*s.perPixel;
+		s.perPixel /= 2;
+	});
+});
+
+setSize();
+render();
